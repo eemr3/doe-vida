8fae4851a3ff883cb0aefd2f70d5b9a2fb7766e4
Emerson Moreira <eemr3@yahoo.com.br>
2026-02-17 20:10:13 -0300

    feat(api): update dependencies and restructure API module
    - Changed project name to 'nestjs-boilerplate' in package.json and package-lock.json.
    - Added new dependencies: @nestjs/config, @nestjs/jwt, @nestjs/passport, @nestjs/typeorm, bcrypt, class-transformer, class-validator, passport, passport-jwt, and pg.
    - Updated devDependencies with @types for bcrypt and passport-jwt.
    - Removed outdated seed-donors-and-donations.sql and related test files.
    - Refactored app.module.ts to include ConfigModule, DatabaseModule, AuthModule, and UserModule.
    - Set global prefix and validation pipes in main.ts for improved API structure.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/users.module';
+import { DatabaseModule } from './shared/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    DatabaseModule,
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
